715a6d8aa5527048146bdd71f13ef206ce416e88
Perez <PPEREZ@ets.org>
2020-01-24 15:41:07 -0500

    'done'

--- src/index.js
+++ src/index.js
@@ -15,167 +15,255 @@ const countries = [{
     brazil:{
       w: 110,
       h: 110,
-      t: 70,
-      l: 5,
+      t: 55,
+      l: 7,
 
     },
     china:{
-      w: 110,
-      h: 110,
-      t: 10,
-      l: 3
+      w: 120,
+      h: 120,
+      t: 8,
+      l: 8
     },
     egypt:{
       w: 110,
-      h: 120
+      h: 110,
+      t: 30,
+      l: 18
     },
     germany:{
-      w: 120,
-      h: 100
+      w: 110,
+      h: 110,
+      t: 63,
+      l: 60.5
     },
     hongkong:{
-      w: 100,
-      h: 100
+      w: 110,
+      h: 110,
+      t: 31,
+      l: 84
     },
     hungary:{
-      w: 130,
-      h: 140
+      w: 100,
+      h: 100,
+      t: 55,
+      l: 72
     },    
     india:{
-      w: 120,
-      h: 100
+      w: 110,
+      h: 110,
+      t: 65,
+      l: 35
     },
     indonesia:{
-      w: 130,
-      h: 140
+      w: 100,
+      h: 100,
+      t: 5,
+      l: 47.1,
     },
     jerusalem:{
       w: 110,
-      h: 120
+      h: 110,
+      t: 4,
+      l: 35
     },
     jordan:{
-      w: 150,
-      h: 140
+      w: 135,
+      h: 135,
+      t: 13,
+      l: 20
     },
     malaysia:{
-      w: 130,
-      h: 140
+      w: 110,
+      h: 110,
+      t: 44,
+      l: 18
     },
     mexico:{
-      w: 120,
-      h: 140
+      w: 135,
+      h: 135,
+      t: 65,
+      l: 47
     },
     pakistan:{
-      w: 120,
-      h: 140
+      w: 135,
+      h: 135,
+      t: 58,
+      l: 20
     },
     palestine:{
-      w: 120,
-      h: 140
+      w: 135,
+      h: 135,
+      t: 23,
+      l: 6
     },
     srilanka:{
       w: 120,
-      h: 140
+      h: 120,
+      t: 5,
+      l: 58
     },
     taiwan:{
-      w: 120,
-      h: 140
+      w: 135,
+      h: 135,
+      t: 21,
+      l: 70
     },
     thailand:{
-      w: 120,
-      h: 140
+      w: 100,
+      h: 100,
+      t: 39.5,
+      l: 7
     },
     turkey:{
-      w: 120,
-      h: 140
+      w: 100,
+      h: 100,
+      t: 7,
+      l: 71
     },
     venezuela:{
-      w: 120,
-      h: 140
+      w: 135,
+      h: 135,
+      t: 38,
+      l : 71
     },
     vietnam:{
-      w: 120,
-      h: 140
-    }
-
+      w: 100,
+      h: 100,
+      t: 17,
+      l: 83             
+    },
 }];
 
 function windowSize(w, h){
-  console.log(w, h)
+  // console.log(w, h)
   const allButtons =  document.querySelectorAll('.bg_img')
 
-  if(w > 950) {
-    document.querySelector('button > img').setAttribute('style', 'display:block');
-    document.querySelector('button span').setAttribute('style', 'display:none');
+  if(w >= 950 ) {
+      document.querySelectorAll('button > img').forEach(img => img.setAttribute('style', 'display:block'))
+    // document.querySelector('button > img').setAttribute('style', 'display:block');
     document.getElementsByTagName('body')[0].setAttribute('style', 'overflow: hidden');
 
     allButtons.forEach((el) => {
      let country = el.getAttribute('data-group')
-     console.log(country);
+    //  console.log(country);
 
      switch (country) {
        case 'brazil':
+          el.firstElementChild.setAttribute('style', 'display:none');
           el.setAttribute(`style`, `width: ${countries[0].brazil.w}px; height:${countries[0].brazil.h}px; background-size:cover; background-position:center;`);
           el.parentElement.setAttribute( `style`,  `top: ${countries[0].brazil.t}%; left: ${countries[0].brazil.l}%;`);
+          if(w <= 1080) el.parentElement.style.left = `${countries[0].brazil.l - 2}%`;
           break;
        case 'china':
+        el.firstElementChild.setAttribute('style', 'display:none');
         el.setAttribute(`style`, `width: ${countries[0].china.w}px; height:${countries[0].china.h}px; background-size:cover; background-position:center;`);
-        console.dir(el)
         el.parentElement.setAttribute( `style`,  `top: ${countries[0].china.t}%; left: ${countries[0].china.l}%;`); 
+        if(w <= 1080) el.parentElement.style.left = `${countries[0].china.l - 4}%`;
+
         break;
        case 'egypt':
-        el.setAttribute(`style`,  `width: ${countries[0].egypt.w}px; height:${countries[0].egypt.h}px; background:${countries[0].egypt.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`,  `width: ${countries[0].egypt.w}px; height:${countries[0].egypt.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].egypt.t}%; left: ${countries[0].egypt.l}%;`);
           break;
        case 'germany':
-        el.setAttribute(`style`, `width: ${countries[0].germany.w}px; height:${countries[0].germany.h}px; background:${countries[0].germany.bg}; background-size:cover; background-position:center;`);
-       break;
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].germany.w}px; height:${countries[0].germany.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].germany.t}%; left: ${countries[0].germany.l}%;`);
+        break;
        case 'hongkong':
-        el.setAttribute(`style`, `width: ${countries[0].hongkong.w}px; height:${countries[0].hongkong.h}px; background:${countries[0].hongkong.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].hongkong.w}px; height:${countries[0].hongkong.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].hongkong.t}%; left: ${countries[0].hongkong.l}%;`);
        break;
        case 'hungary':
-        el.setAttribute(`style`, `width: ${countries[0].hungary.w}px; height:${countries[0].hungary.h}px; background:${countries[0].hungary.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].hungary.w}px; height:${countries[0].hungary.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].hungary.t}%; left: ${countries[0].hungary.l}%;`);
        break;
        case 'india':
-        el.setAttribute(`style`, `width: ${countries[0].india.w}px; height:${countries[0].india.h}px; background:${countries[0].india.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].india.w}px; height:${countries[0].india.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].india.t}%; left: ${countries[0].india.l}%;`);
+        if(w <= 1080) el.parentElement.style.left = `${countries[0].india.l - 3}%`;
+
        break;
        case 'indonesia':
-        el.setAttribute(`style`, `width: ${countries[0].indonesia.w}px; height:${countries[0].indonesia.h}px; background:${countries[0].indonesia.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].indonesia.w}px; height:${countries[0].indonesia.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].indonesia.t}%; left: ${countries[0].indonesia.l}%;`);
+        if(w <= 1080) el.parentElement.style.left = `${countries[0].indonesia.l - 3}%`;
+
        break;
        case 'jerusalem':
-        el.setAttribute(`style`, `width: ${countries[0].jerusalem.w}px; height:${countries[0].jerusalem.h}px; background:${countries[0].jerusalem.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].jerusalem.w}px; height:${countries[0].jerusalem.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].jerusalem.t}%; left: ${countries[0].jerusalem.l}%;`);
+        if(w <= 1080) el.parentElement.style.left = `${countries[0].jerusalem.l - 6}%`;
+
        break;
        case 'jordan':
-        el.setAttribute(`style`, `width: ${countries[0].jordan.w}px; height:${countries[0].jordan.h}px; background:${countries[0].jordan.bg}; background-size:cover; background-position:center;`);
-       break;
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].jordan.w}px; height:${countries[0].jordan.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].jordan.t}%; left: ${countries[0].jordan.l}%;`);        
+        if(w <= 1080) el.parentElement.style.left = `${countries[0].jordan.l - 4}%`;
+        break;
        case 'malaysia':
-        el.setAttribute(`style`, `width: ${countries[0].malaysia.w}px; height:${countries[0].malaysia.h}px; background:${countries[0].malaysia.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].malaysia.w}px; height:${countries[0].malaysia.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].malaysia.t}%; left: ${countries[0].malaysia.l}%;`);
        break;
        case 'mexico':
-        el.setAttribute(`style`, `width: ${countries[0].mexico.w}px; height:${countries[0].mexico.h}px; background:${countries[0].mexico.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].mexico.w}px; height:${countries[0].mexico.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].mexico.t}%; left: ${countries[0].mexico.l}%;`);
+        if(w <= 1080) el.parentElement.style.left = `${countries[0].mexico.l - 2}%`;
+
        break;
        case 'pakistan':
-        el.setAttribute(`style`, `width: ${countries[0].pakistan.w}px; height:${countries[0].pakistan.h}px; background:${countries[0].pakistan.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].pakistan.w}px; height:${countries[0].pakistan.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].pakistan.t}%; left: ${countries[0].pakistan.l}%;`);
+        if(w <= 1080) el.parentElement.style.left = `${countries[0].pakistan.l - 4}%`;
+
        break;
        case 'palestine':
-        el.setAttribute(`style`, `width: ${countries[0].palestine.w}px; height:${countries[0].palestine.h}px; background:${countries[0].palestine.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].palestine.w}px; height:${countries[0].palestine.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].palestine.t}%; left: ${countries[0].palestine.l}%;`);
+        if(w <= 1080) el.parentElement.style.left = `${countries[0].palestine.l - 4}%`;
+
        break;
        case 'srilanka':
-        el.setAttribute(`style`, `width: ${countries[0].srilanka.w}px; height:${countries[0].srilanka.h}px; background:${countries[0].srilanka.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].srilanka.w}px; height:${countries[0].srilanka.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].srilanka.t}%; left: ${countries[0].srilanka.l}%;`);
        break;
        case 'taiwan':
-        el.setAttribute(`style`, `width: ${countries[0].taiwan.w}px; height:${countries[0].taiwan.h}px; background:${countries[0].taiwan.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].taiwan.w}px; height:${countries[0].taiwan.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].taiwan.t}%; left: ${countries[0].taiwan.l}%;`);
        break;
        case 'thailand':
-        el.setAttribute(`style`, `width: ${countries[0].thailand.w}px; height:${countries[0].thailand.h}px; background:${countries[0].thailand.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].thailand.w}px; height:${countries[0].thailand.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].thailand.t}%; left: ${countries[0].thailand.l}%;`);
        break;
        case 'turkey':
-        el.setAttribute(`style`, `width: ${countries[0].turkey.w}px; height:${countries[0].turkey.h}px; background:${countries[0].turkey.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].turkey.w}px; height:${countries[0].turkey.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].turkey.t}%; left: ${countries[0].turkey.l}%;`);
        break;
        case 'venezuela':
-        el.setAttribute(`style`, `width: ${countries[0].venezuela.w}px; height:${countries[0].venezuela.h}px; background:${countries[0].venezuela.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].venezuela.w}px; height:${countries[0].venezuela.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].venezuela.t}%; left: ${countries[0].venezuela.l}%;`);
        break;
        case 'vietnam':
-        el.setAttribute(`style`, `width: ${countries[0].vietnam.w}px; height:${countries[0].vietnam.h}px; background:${countries[0].vietnam.bg}; background-size:cover; background-position:center;`);
+        el.firstElementChild.setAttribute('style', 'display:none');
+        el.setAttribute(`style`, `width: ${countries[0].vietnam.w}px; height:${countries[0].vietnam.h}px; background-size:cover; background-position:center;`);
+        el.parentElement.setAttribute( `style`,  `top: ${countries[0].vietnam.t}%; left: ${countries[0].vietnam.l}%;`);
        break;
        default:
          break;
@@ -186,8 +274,8 @@ function windowSize(w, h){
   } else {
     allButtons.forEach( (el) => {
       el.removeAttribute('style');
-      document.querySelector('button span').setAttribute('style', 'display:block');
-      document.querySelector('button > img').setAttribute('style', 'display:none');
+      el.children[0].setAttribute('style', 'display:block');
+      el.children[1].setAttribute('style', 'display:none' );
       document.getElementsByTagName('body')[0].setAttribute('style', 'overflow: visible');
 
     })
@@ -201,6 +289,7 @@ window.addEventListener('load', reLoad);
 function reSize(){
   let width = this.innerWidth;
   let height = this.innerHeight;
+
   windowSize(width, height)
 }
 
